c09ca8adf81c3008aa1c71d1877f0582a473ef26
faisal-al-fatha <faysal313@gmail.com>
2022-11-10 13:29:26 +0600

    review card added

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,8 +1,24 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Layout/Main";
 
 const MyReviews = () => {
+    const {user} = useContext(AuthContext);
+    const [reviews, setReviews] = useState({});
+    const url = `http://localhost:5000/reviews?email=${user?.email}`;
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email])
+    console.log(reviews);
     return (
         <div>
-            
+
+            you have {reviews.length}
+           {/* {
+            reviews.map(review=><ReviewsCard review ={review}></ReviewsCard>)
+           } */}
         </div>
     );
 };
